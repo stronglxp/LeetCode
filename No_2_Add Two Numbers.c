b0fa6e6ca7b78a3e6e56e3cd89651b5bb6c19ed3
liuxiaoping <18252025311@163.com>
2019-09-01 17:14:44 +0800

    create No_2

---
+++ No_2_Add Two Numbers.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    
+    struct ListNode *r, *t, *node;
+    int num = 0;
+    int val = 0;
+
+    r = (struct ListNode *)malloc(sizeof(struct ListNode));
+    r->next = NULL;
+    t = r;
+    
+    while (l1 != NULL || !l2 != NULL || num != 0) {
+
+        node = (struct ListNode *)malloc(sizeof(struct ListNode));  
+        val = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + num;
+        
+        node->val = val % 10;
+        node->next = NULL;
+        num = val / 10;
+
+        t->next = node;
+        t = node;
+
+        l1 = l1 ? l1->next : l1;
+        l2 = l2 ? l2->next : l2;
+    }
+    
+    t = r->next;
+    free(r);
+    return t;
+}
+
